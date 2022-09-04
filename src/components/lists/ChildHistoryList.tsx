b7265bd4bb991969806ca045a5adfd59f69d54d8
HitomiWin <hitomi.winberg@gmail.com>
2022-09-04 16:34:40 +0200

    add div and background-color

--- src/components/lists/ChildHistoryList.tsx
+++ src/components/lists/ChildHistoryList.tsx
@@ -1,5 +1,19 @@
 import React, { memo, VFC, useRef } from "react";
-import { Row, Col, Card, Button, Form, Spinner, Alert } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Card,
+  Button,
+  Form,
+  Spinner,
+  Alert,
+  Container,
+  OverlayTrigger,
+  Popover,
+} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+
 import { useParams } from "react-router-dom";
 import { DocumentData, serverTimestamp } from "firebase/firestore";
 import moment from "moment";
@@ -21,6 +35,15 @@ export const ChildHistoryList: VFC = memo(() => {
   const transactionsQuery = useAddTransactions(id ?? "");
   const transActions = useGetTransactions(id ?? "");
   const totalAmount = useGetTotalAmount(id ?? "");
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>
+        Keep track of your spending below by entering the anount. Any
+        withdrawals can simply be entered with a minus sign in front of the sum
+      </Popover.Body>
+    </Popover>
+  );
   const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const isRegular = false;
@@ -56,28 +79,43 @@ export const ChildHistoryList: VFC = memo(() => {
     priceRef.current.value = "";
   }
   return transactionsQuery && child && child.data ? (
-    <>
+    <Container>
       <Row>
         <Col
           xs={{ span: 12 }}
           md={{ span: 8, offset: 2 }}
           lg={{ span: 6, offset: 3 }}
         >
-          <Row className="d-f align-items-center justify-content-between px-2">
+          <Row className="d-f align-items-center justify-content-between p-0 m-0 bg-info rounded">
             <Col xs={{ span: 5 }}>
-              <h3>{child.data.name}</h3>
+              <h3 className="mt-3">{child.data.name}</h3>
             </Col>
             <Col xs={{ span: 5 }} className="text-end">
-              <h4>{totalAmount} kr</h4>
+              <h4 className="mt-3">{totalAmount} kr</h4>
             </Col>
           </Row>
-          <Card className="mt-3">
+          <div className="mt-3 bg-info rounded">
             <Card.Body>
               {transactionsQuery.isError && (
                 <Alert variant="danger"> {transactionsQuery.error} </Alert>
               )}
               <Card.Title className="text-secondary text-center mb-4">
-                Deposit or withdraw amount
+                <p> Deposit or withdraw amount &nbsp;</p>
+                <OverlayTrigger
+                  trigger="click"
+                  key="right"
+                  placement="right"
+                  overlay={popover}
+                >
+                  <p>
+                    <FontAwesomeIcon
+                      icon={faInfoCircle}
+                      color="#rgb(23, 23, 77)"
+                      size="sm"
+                      className="hover-icon circle-info"
+                    />
+                  </p>
+                </OverlayTrigger>
               </Card.Title>
               <Form onSubmit={handleOnSubmit}>
                 <Form.Group controlId="formBasicEmail">
@@ -85,7 +123,7 @@ export const ChildHistoryList: VFC = memo(() => {
                     <Col md={11} lg={10}>
                       <Form.Control
                         type="number"
-                        placeholder="Enter amount"
+                        placeholder="Enter amount ex. 10, -10"
                         ref={priceRef}
                       />
                     </Col>
@@ -105,26 +143,29 @@ export const ChildHistoryList: VFC = memo(() => {
                 </Form.Group>
               </Form>
             </Card.Body>
-          </Card>
+          </div>
+          <div className="bg-info rounded">
+            <h3 className="text-center my-4">
+              History of deposit or withdraw{" "}
+            </h3>
+            <Row className="justify-content-center">
+              {transActions.isError && (
+                <Alert variant="danger"> {transActions.error} </Alert>
+              )}
+              {transActions.isLoading && <p>Loading...</p>}
 
-          <h3 className="text-center my-4">History of deposit or withdraw </h3>
-          <Row className="justify-content-center">
-            {transActions.isError && (
-              <Alert variant="danger"> {transActions.error} </Alert>
-            )}
-            {transActions.isLoading && <p>Loading...</p>}
-
-            {transActions && transActions.data && transActions.data.length ? (
-              transActions.data.map((transaction) => (
-                <HistoryCard key={transaction.id} transaction={transaction} />
-              ))
-            ) : (
-              <h4 className="text-center my-4">No history</h4>
-            )}
-          </Row>
+              {transActions && transActions.data && transActions.data.length ? (
+                transActions.data.map((transaction) => (
+                  <HistoryCard key={transaction.id} transaction={transaction} />
+                ))
+              ) : (
+                <h4 className="text-center my-4">No history</h4>
+              )}
+            </Row>
+          </div>
         </Col>
       </Row>
-    </>
+    </Container>
   ) : (
     <p className="text-center">No Child</p>
   );
