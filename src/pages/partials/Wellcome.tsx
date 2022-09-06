8966d84c0a98dddc003d5b2b5c3a9ca9b6bff999
HitomiWin <hitomi.winberg@gmail.com>
2022-09-06 15:23:51 +0200

    add welcome text

---
+++ src/pages/partials/Wellcome.tsx
@@ -0,0 +1,9 @@
+export const Wellcome = () => {
+  return (
+    <div>
+      <h3 className="flowing text-center">
+        Oink Bank makes it easy to track allowances for kids!
+      </h3>
+    </div>
+  );
+};
